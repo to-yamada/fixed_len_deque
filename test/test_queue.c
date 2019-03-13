98c94f064d3dcbc944cbda0c8ebd4e1c8533bc2a
to-yamada <5902143+to-yamada@users.noreply.github.com>
2019-03-13 12:37:39 +0900

    Remove test cases for unsigned char queue

--- test/test_queue.c
+++ test/test_queue.c
@@ -1,9 +1,6 @@
 #include "PCUnit/PCUnit.h"
 #include "../queue.h"
 
-static unsigned char uc_data[5];
-QUEUE uc_queue;
-
 typedef struct {
 	int x;
 	int y;
@@ -23,7 +20,6 @@ static int cleanup(void)
 
 static int setup(void)
 {
-	INIT_QUEUE_ARRAY(&uc_queue, uc_data);
 	INIT_QUEUE_ARRAY(&point_queue, point_data);
 	return 0;
 }
@@ -33,123 +29,7 @@ static int teardown(void)
 	return 0;
 }
 
-
-static void test_enqueue_dequeue_single_uc(void)
-{
-	unsigned char data = 0;
-	const unsigned char expected = 10;
-
-	PCU_ASSERT_TRUE(enqueue_queue(&uc_queue, &expected));
-
-	PCU_ASSERT_TRUE(dequeue_queue(&uc_queue, &data));
-	PCU_ASSERT_EQUAL(expected, data);
-}
-
-static void test_enqueue_dequeue_multi_uc(void)
-{
-	const unsigned char expected[] = {10, 20, 30, 40, 50};
-	int i = 0;
-
-	for (i = 0; i < 5; i++) {
-		PCU_ASSERT_TRUE(enqueue_queue(&uc_queue, &expected[i]));
-	}
-
-	for (i = 0; i < 5; i++) {
-		unsigned char data = 0;
-		PCU_ASSERT_TRUE(dequeue_queue(&uc_queue, &data));
-		PCU_ASSERT_EQUAL(expected[i], data);
-	}
-}
-
-static void test_enqueue_full_uc(void)
-{
-	unsigned char data = 0;
-	int i = 0;
-
-	for (i = 0; i < 5; i++) {
-		PCU_ASSERT_TRUE(enqueue_queue(&uc_queue, &data));
-	}
-	PCU_ASSERT_FALSE(enqueue_queue(&uc_queue, &data));
-}
-
-static void test_dequeue_empty_uc(void)
-{
-	unsigned char data = 0;
-	PCU_ASSERT_FALSE(dequeue_queue(&uc_queue, &data));
-}
-
-static void test_dequeue_more_than_num_enqueues_uc(void)
-{
-	unsigned char data = 0;
-
-	PCU_ASSERT_TRUE(enqueue_queue(&uc_queue, &data));
-
-	PCU_ASSERT_TRUE(dequeue_queue(&uc_queue, &data));
-	PCU_ASSERT_FALSE(dequeue_queue(&uc_queue, &data));
-}
-
-static void test_enqueue_at_single_uc(void)
-{
-	unsigned char data = 0;
-	const unsigned char expected = 10;
-
-	PCU_ASSERT_TRUE(enqueue_queue(&uc_queue, &expected));
-
-	PCU_ASSERT_TRUE(at_queue(&uc_queue, 0, &data));
-	PCU_ASSERT_EQUAL(expected, data);
-}
-
-static void test_enqueue_at_multi_uc(void)
-{
-	const unsigned char expected[] = {10, 20, 30, 40, 50};
-	int i = 0;
-
-	for (i = 0; i < 5; i++) {
-		PCU_ASSERT_TRUE(enqueue_queue(&uc_queue, &expected[i]));
-	}
-
-	for (i = 0; i < 5; i++) {
-		unsigned char data = 0;
-		PCU_ASSERT_TRUE(at_queue(&uc_queue, i, &data));
-		PCU_ASSERT_EQUAL(expected[i], data);
-	}
-}
-
-static void test_at_empty_uc(void)
-{
-	unsigned char data = 0;
-
-	PCU_ASSERT_FALSE(at_queue(&uc_queue, 0, &data));
-}
-
-static void test_at_more_than_num_enqueues_uc(void)
-{
-	unsigned char data = 0;
-
-	PCU_ASSERT_TRUE(enqueue_queue(&uc_queue, &data));
-
-	PCU_ASSERT_TRUE(at_queue(&uc_queue, 0, &data));
-	PCU_ASSERT_TRUE(at_queue(&uc_queue, 0, &data));
-}
-
-static void test_at_invalid_index_uc(void)
-{
-	unsigned char data = 0;
-
-	PCU_ASSERT_TRUE(enqueue_queue(&uc_queue, &data));
-
-	PCU_ASSERT_FALSE(at_queue(&uc_queue, 1, &data));
-}
-
-static void test_at_NULL_uc(void)
-{
-	unsigned char data = 0;
-
-	enqueue_queue(&uc_queue, &data);
-	PCU_ASSERT_TRUE(at_queue(&uc_queue, 0, NULL));
-}
-
-static void test_enqueue_dequeue_single_point(void)
+static void test_enqueue_dequeue_single(void)
 {
 	POINT data = {0};
 	const POINT expected = {-1000, 256};
@@ -160,7 +40,7 @@ static void test_enqueue_dequeue_single_point(void)
 	PCU_ASSERT_MEMORY_EQUAL(&expected, &data, sizeof(POINT), 1);
 }
 
-static void test_enqueue_dequeue_multi_point(void)
+static void test_enqueue_dequeue_multi(void)
 {
 	const POINT expected[] = {
 		{ 1000,  -1000},
@@ -187,7 +67,7 @@ static void test_enqueue_dequeue_multi_point(void)
 	}
 }
 
-static void test_enqueue_full_point(void)
+static void test_enqueue_full(void)
 {
 	POINT data = {0};
 	int i = 0;
@@ -198,13 +78,13 @@ static void test_enqueue_full_point(void)
 	PCU_ASSERT_FALSE(enqueue_queue(&point_queue, &data));
 }
 
-static void test_dequeue_empty_point(void)
+static void test_dequeue_empty(void)
 {
 	POINT data = {0};
 	PCU_ASSERT_FALSE(dequeue_queue(&point_queue, &data));
 }
 
-static void test_dequeue_more_than_num_enqueues_point(void)
+static void test_dequeue_more_than_num_enqueues(void)
 {
 	POINT data = {0};
 
@@ -214,7 +94,7 @@ static void test_dequeue_more_than_num_enqueues_point(void)
 	PCU_ASSERT_FALSE(dequeue_queue(&point_queue, &data));
 }
 
-static void test_enqueue_at_single_point(void)
+static void test_enqueue_at_single(void)
 {
 	POINT data = {0};
 	const POINT expected = {-1000, 256};
@@ -225,7 +105,7 @@ static void test_enqueue_at_single_point(void)
 	PCU_ASSERT_MEMORY_EQUAL(&expected, &data, sizeof(POINT), 1);
 }
 
-static void test_enqueue_at_multi_point(void)
+static void test_enqueue_at_multi(void)
 {
 	const POINT expected[] = {
 		{ 1000,  -1000},
@@ -252,14 +132,14 @@ static void test_enqueue_at_multi_point(void)
 	}
 }
 
-static void test_at_empty_point(void)
+static void test_at_empty(void)
 {
 	POINT data = {0};
 
 	PCU_ASSERT_FALSE(at_queue(&point_queue, 0, &data));
 }
 
-static void test_at_more_than_num_enqueues_point(void)
+static void test_at_more_than_num_enqueues(void)
 {
 	POINT data = {0};
 
@@ -269,7 +149,7 @@ static void test_at_more_than_num_enqueues_point(void)
 	PCU_ASSERT_TRUE(at_queue(&point_queue, 0, &data));
 }
 
-static void test_at_invalid_index_point(void)
+static void test_at_invalid_index(void)
 {
 	POINT data = {0};
 
@@ -278,7 +158,7 @@ static void test_at_invalid_index_point(void)
 	PCU_ASSERT_FALSE(at_queue(&point_queue, 1, &data));
 }
 
-static void test_at_NULL_point(void)
+static void test_at_NULL(void)
 {
 	POINT data = {0};
 
